c282028d72d865c99a25ea7f5e33492306c95a01
Ivan Kleshnin <ivan@paqmind.com>
2022-11-28 18:06:46 +0300

    Simplify content search rules in TW config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
 	content: [
-		"./layout/**/*.{js,ts,jsx,tsx}",
-
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,jsx}",
+		"./layout/**/*.{js,jsx}",
+		"./pages/**/*.{js,jsx}",
 	],
 	theme: {
 		extend: {
