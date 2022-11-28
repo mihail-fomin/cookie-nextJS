7aa6fc063beb2a07517b56d6f4f32a9dccb5263d
Ivan Kleshnin <ivan@paqmind.com>
2022-11-28 18:04:07 +0300

    Add comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,17 +12,34 @@ module.exports = {
 				redColor: '#b5363e',
 				color300: '#efc8cb',
 				color400: '#e9b4b8',
-				color200: '#F3D8DA',
+
+				// TODO apply `red` palette
+				// red: {
+				//    // https://color.adobe.com/create/color-wheel (rotate from hue:0 to hue:350)
+        //    50: "#FFF2F4",
+        //   100: "#FFE3E8",
+        //   200: "#FFCCD5",
+        //   300: "#FCA4B3",
+        //   400: "#F77288",
+        //   500: "#F04360",
+        //   600: "#DB2544",
+        //   700: "#BA1C36",
+        //   800: "#991C30",
+        //   900: "#801D2E",
+        // },
 			},
+
 			dropShadow: {
 				'xl': '0 8px 5px rgb(181, 54, 62)',
 			},
 		},
+
 		fontFamily: {
 			logo: ['Kaushan Script', 'cursive'],
 			sans: ['Montserrat', 'sans-serif'],
 			fancy: ['Lobster', 'cursive'],
 		},
+
 		screens: {
 			sm: '480px',
 			md: '768px',
@@ -30,5 +47,6 @@ module.exports = {
 			xl: '1440px',
 		}
 	},
+
 	plugins: [],
 }
