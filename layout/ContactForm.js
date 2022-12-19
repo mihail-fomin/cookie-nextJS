e08b8d8393bf23705190e942776fba98c55889c7
misha133kv <mihail_fomin_95@mail.ru>
2022-12-19 20:41:15 +0500

    edited form

--- layout/ContactForm.js
+++ layout/ContactForm.js
@@ -7,8 +7,15 @@ export default function ContactForm() {
 	const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
 	const [name, setName] = useState('');
+	const [phone, setPhone] = useState('');
+	const [text, setText] = useState('');
 	const [nameDirty, setNameDirty] = useState(false);
+	const [phoneDirty, setPhoneDirty] = useState(false);
+	const [textDirty, setTextDirty] = useState(false);
 	const [nameError, setNameError] = useState('');
+	const [phoneError, setPhoneError] = useState('');
+	const [textError, setTextError] = useState('');
+	const [formValid, setformValid] = useState(false);
 
 	const onNameChange = React.useCallback((value) => {
 		if (!nameDirty) {
@@ -17,6 +24,23 @@ export default function ContactForm() {
 		setName(value)
 		setNameError(validateName(value))
 	}, [nameDirty])
+
+	const onPhoneChange = React.useCallback((value) => {
+		if (!phoneDirty) {
+			setPhoneDirty(true)
+		}
+		setPhone(value)
+		setPhoneError(validatePhone(value))
+	}, [phoneDirty])
+
+	const onTextChange = React.useCallback((value) => {
+		if (!textDirty) {
+			setTextDirty(true)
+		}
+		setText(value)
+		setTextError(validateText(value))
+	}, [textDirty])
+
 
 	let formatBody = ({ sender, phone, orderText }) => `
 	<h4>Заявка с сайта</h4>
@@ -33,18 +57,34 @@ export default function ContactForm() {
 				setNameDirty(true)
 				break
 		}
+		switch (e.target.id) {
+			case 'phone':
+				setNameDirty(true)
+				break
+		}
+		switch (e.target.id) {
+			case 'text':
+				setTextDirty(true)
+				break
+		}
 	}, [])
 
-	const handleSubmit = e => {
+	const handleSubmit = () => {
 		// TODO reset ALL dirty, ALL errors, etc. to false if we don't redirect
 
-		e.preventDefault();
-		const data = {
-			name,
-			phone,
-			text,
-		};
 
+		let formatBody = ({ sender, phone, orderText }) => `
+  			<strong>Заявка с сайта</strong>n/
+    		<b>Отправитель:</b> ${name}n/
+    		<b>Телефон:</b> ${phone}n/
+    		<b>Текст заказа:</b> ${text}
+`
+
+		let body = formatBody({
+			sender: this.name.value,
+			phone: this.phone.value,
+			orderText: this.text.value,
+		})
 
 		fetch(`https://api.telegram.org/bot5775183225:AAGUPuyf5PHRfSa5Zoux-zz5_KWIx1vHAPo/sendMessage`, {
 			method: "POST",
@@ -53,12 +93,24 @@ export default function ContactForm() {
 			},
 			body: JSON.stringify({
 				chat_id: "-1001759583869",
-				text: "<b>test</b>",
+				text: formatBody,
 				parse_mode: "html",
 			})
 		})
 			.then(resp => {
 				return resp.json()
+				setName('');
+				setPhone('');
+				setText('');
+				setNameDirty(false);
+				setPhoneDirty(false);
+				setTextDirty(false);
+			})
+			.catch((err) => {
+				console.warn(err);
+			})
+			.finally(() => {
+				console.log('Конец');
 			})
 	};
 
@@ -75,9 +127,24 @@ export default function ContactForm() {
 					blurHandler={blurHandler}
 					onChange={onNameChange}
 				/>
+				<PhoneField
+					phoneDirty={phoneDirty}
+					phoneError={phoneError}
+					phone={phone}
+					blurHandler={blurHandler}
+					onChange={onPhoneChange}
+				/>
+				<TextField
+					textDirty={phoneDirty}
+					textError={textError}
+					text={text}
+					blurHandler={blurHandler}
+					onChange={onTextChange}
+				/>
 				<button
 					className='block p-2 border border-red-700'
 					type="submit"
+					onClick={handleSubmit}
 				>
 					Send
 				</button>
@@ -87,10 +154,30 @@ export default function ContactForm() {
 }
 
 function validateName(name) {
+	const re = /^[a-zA-Z'][a-zA-Z-' ]+[a-zA-Z']?$/u
 	if (!name) {
 		return "Заполните поле"
-	} else if ((String(e.target.value)).length < 3) {
-		return "Введите хотябы 3 буквы("
+	} else if (!re.test(String(name))) {
+		return "Введите только буквы"
+	} else {
+		return ""
+	}
+}
+
+function validatePhone(phone) {
+	const re = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/u
+	if (!phone) {
+		return "Заполните поле"
+	} else if (!re.test((phone))) {
+		return ('Введите корректный телефон')
+	} else {
+		return ""
+	}
+}
+
+function validateText(text) {
+	if (!text) {
+		return "Заполните поле"
 	} else {
 		return ""
 	}
@@ -101,10 +188,41 @@ const NameField = React.memo(({ nameDirty, nameError, name, onBlur, onChange }) 
 		<label htmlFor="name">Имя:</label>
 		{(nameDirty && nameError) && <div className='text-red-800'>{nameError}</div>}
 		<input
-			className='block'
+			className='block p-2'
 			id="name"
 			type="text"
 			value={name}
+			onBlur={onBlur}
+			onChange={event => onChange(event.target.value)}
+		/>
+	</>
+})
+
+const PhoneField = React.memo(({ phoneDirty, phoneError, phone, onBlur, onChange }) => {
+	return <>
+		<label htmlFor="phone">Телефон:</label>
+		{(phoneDirty && phoneError) && <div className='text-red-800'>{phoneError}</div>}
+		<input
+			className='block p-2'
+			id="phone"
+			type="tel"
+			value={phone}
+			onBlur={onBlur}
+			onChange={event => onChange(event.target.value)}
+		/>
+	</>
+})
+
+const TextField = React.memo(({ textDirty, textError, text, onBlur, onChange }) => {
+	return <>
+		<label htmlFor="text">Введите текст заказа:</label>
+		{(textDirty && textError) && <div className='text-red-800'>{textError}</div>}
+		<textarea
+			className='block min-h-[150px] p-2'
+			id="text"
+			type="text"
+			placeholder=''
+			value={text}
 			onBlur={onBlur}
 			onChange={event => onChange(event.target.value)}
 		/>
